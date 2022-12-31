8b3bda4cafbb74878978e68cf66f1e93d7b39f27
Georgeb779 <georgabez123@hotmail.com>
2022-12-30 20:59:48 -0400

    feat: create navbar component and add funtionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,27 @@
+import { motion } from 'framer-motion';
+
+import { NavBar } from '@/components';
+
 const Index = () => {
+  const variants = {
+    hidden: {
+      scale: 0.8,
+      opacity: 0,
+    },
+    visible: {
+      scale: 1,
+      opacity: 1,
+      transition: {
+        delay: 0.4,
+      },
+    },
+  };
+
   return (
     <>
-      <div>
-        <h1>Hello petter</h1>
-      </div>
+      <motion.div initial="hidden" animate="visible" variants={variants}>
+        <NavBar />
+      </motion.div>
     </>
   );
 };
