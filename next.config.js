dbab81337bb94984661c3e8d0924339e8d37fd1c
Georgeb779 <georgebaez123@hotmail.com>
2023-01-02 16:10:59 -0400

    chore: add and configure svgr/webpack

--- next.config.js
+++ next.config.js
@@ -15,3 +15,16 @@ module.exports = withBundleAnalyzer({
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
 });
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
