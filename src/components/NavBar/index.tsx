2519c7217558c0e5f1726efc250cac918754d604
Georgeb779 <georgebaez123@hotmail.com>
2023-01-03 19:10:12 -0400

    feat: add close sidebar when user click outside the sidebar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,8 +1,6 @@
-import { motion, useCycle } from 'framer-motion';
+import { motion } from 'framer-motion';
 import Link from 'next/link';
 import * as React from 'react';
-
-import { useDimensions } from '@/utils';
 
 import { MenuToggle } from '../MenuToggle';
 import { SidebarItems } from '../SidebarItems';
@@ -10,32 +8,41 @@ import SocialSection from '../SocialSection';
 import style from './style.module.scss';
 
 export const NavBar = () => {
-  const [isOpen, cycleOpen] = useCycle(false, true);
-  const containerRef = React.useRef(null);
-  const { height } = useDimensions(containerRef);
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    const main = document.querySelector('main');
+    if (main) {
+      main.addEventListener('click', () => {
+        if (isOpen) {
+          setIsOpen(false);
+        }
+      });
+    }
+  }, [isOpen]);
 
   return (
     <>
       <motion.nav className={style.navbar__container}>
         <div className={style.navbar__logo}>
-          <Link href="/">G</Link>
+          <Link href="/" onClick={() => setIsOpen(false)}>
+            G
+          </Link>
         </div>
         <motion.div
           initial={false}
           animate={isOpen ? 'open' : 'closed'}
-          custom={height}
-          ref={containerRef}
           className="background"
         >
           <motion.button
-            onClick={() => cycleOpen()}
+            onClick={() => setIsOpen(!isOpen)}
             title="Toggle Menu"
             type="button"
             className={style.toggle_btn}
           >
             <MenuToggle isOpen={isOpen} />
           </motion.button>
-          <SidebarItems />
+          <SidebarItems isOpen={isOpen} setIsOpen={setIsOpen} />
         </motion.div>
         <SocialSection />
       </motion.nav>
--- src/components/SidebarItems/index.tsx
+++ src/components/SidebarItems/index.tsx
@@ -7,7 +7,13 @@ import { itemVariants } from '@/animations';
 
 import style from './style.module.scss';
 
-export const SidebarItems = () => {
+export const SidebarItems = ({
+  setIsOpen,
+  isOpen,
+}: {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+}) => {
   const routes = [
     { name: 'Home', url: '/' },
     { name: 'About', url: '/about' },
@@ -25,7 +31,7 @@ export const SidebarItems = () => {
           position: 'absolute',
           visibility: 'visible',
           opacity: 1,
-          x: 49,
+          x: 45,
           transition: {
             delay: 0.3,
             stiffness: 20,
@@ -54,8 +60,9 @@ export const SidebarItems = () => {
             key={index}
             variants={itemVariants(index)}
             className={asPath === route.url ? style.active : ''}
+            onClick={() => setIsOpen(!isOpen)}
           >
-            <Link href={route.url}> {route.name}</Link>
+            <Link href={route.url}>{route.name}</Link>
           </motion.li>
         ))}
       </motion.ul>
